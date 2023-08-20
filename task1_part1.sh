e11227d96b9b3cd87e52885e382adda30b21933b
Rami Barakat <77164176+RamiBarakat@users.noreply.github.com>
2023-08-20 09:43:38 +0300

    Create task1_part1.sh

---
+++ task1_part1.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+
+fdisk /dev/sdb <<EOF
+n
+p
+1
+
+
+t
+8e
+w
+EOF
+
+
+
+#create a PV
+sudo pvcreate /dev/sdb1
+
+#create a VG with extent size as 16MB
+sudo vgcreate -s 16MB vg1 /dev/sdb1
+
+#create a volumegroup 
+sudo lvcreate -l 50 vg1 -n lv1
+
+#set file system to ext4
+sudo mkfs.ext4 -m 0 /dev/vg1/lv1
+
+sudo mkdir /mnt/data
+
+sudo mount /dev/vg1/lv1 /mnt/data
+
+#automatic mounting
+echo "/dev/vg1/lv1      /mnt/data       ext4    defaults        0       0" >> /etc/fstab
+
+mount -a
