010fd1d70975fe2f3c849442c1acd1d41cea8fd4
Rami Barakat <77164176+RamiBarakat@users.noreply.github.com>
2023-08-20 12:14:20 +0300

    Update task2_init.sh

--- task2_init.sh
+++ task2_init.sh
@@ -10,5 +10,21 @@ echo "1 * * * * /tmp/task2/calcavgs.sh" >> /tmp/mycron
 
 crontab /tmp/mycron
 
+cat << EOF > /var/www/html/index.html
+<!DOCTYPE html>
+<html>
+<head>
+    <title>System Statistics</title>
+</head>
+<body>
+    <h1>System Statistics</h1>
+    <ul>
+        <li><a href="cpu.html">CPU Usage</a></li>
+        <li><a href="memory.html">Memory Usage</a></li>
+        <li><a href="disk.html">Disk Usage</a></li>
+    </ul>
+</body>
+</html>
+EOF
 
-
+lynx /var/www/html/index.html
