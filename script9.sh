5daf6745ca9f8cba26f5da686ea22b91692cc8e0
Rami Barakat <77164176+RamiBarakat@users.noreply.github.com>
2023-08-21 12:59:36 +0300

    Update script9.sh

--- script9.sh
+++ script9.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
 
-who | cut -d ' ' -f 1,10,11 > ec.txt
-dash=" - "
-rm final.txt
-
-
-while read -r line; do
-
-        string=$( echo $line | cut -d ' ' -f 2,3 )
-        name=$(echo $line | cut -d ' ' -f 1)
-        string+=$dash
-        string+=$name
-        echo "$string" >> final.txt
-
-done < ec.txt
+var=$(who | awk '{ print $3, $4, "-" , $1 }')
+echo $var
